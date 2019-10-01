99856ce194a8fdcd07657d75f49af3f7ed27e607
Xujing Cai <csuzie93@gmail.com>
2019-09-30 20:54:38 -0400

    fixed navigation bug

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React, { Fragment } from "react";
-import { View, Text, YellowBox } from "react-native";
-import Navigation from "./src/Navigation/Navigation";
+import React, {Fragment} from 'react';
+import {View, Text, YellowBox} from 'react-native';
+import Navigation from './src/Navigation/Navigation';
 
-YellowBox.ignoreWarnings(["Remote de"]);
+YellowBox.ignoreWarnings(['Remote de']);
 
 const App = () => {
   return <Navigation />;
